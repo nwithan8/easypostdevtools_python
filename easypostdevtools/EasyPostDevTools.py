b5bab4ec74033823168d5b40ee9e09fb168e3d68
Nate Harris <nharris@easypost.com>
2021-12-21 13:09:52 -0500

    Allow use maps when creating shipment

--- easypostdevtools/EasyPostDevTools.py
+++ easypostdevtools/EasyPostDevTools.py
@@ -221,13 +221,13 @@ class EasyPostDevTools:
             return _map
 
         @staticmethod
-        def get() -> easypost.Shipment:
-            _map = EasyPostDevTools.Shipments.get_map(None, None, None)
+        def get(to_address_map: dict = None, from_address_map: dict = None, parcel_map: dict = None) -> easypost.Shipment:
+            _map = EasyPostDevTools.Shipments.get_map(to_address_map, from_address_map, parcel_map)
             return easypost.Shipment.create(**_map)
 
         @staticmethod
-        def get_return() -> easypost.Shipment:
-            _map = EasyPostDevTools.Shipments.get_return_map(None, None, None)
+        def get_return(to_address_map: dict = None, from_address_map: dict = None, parcel_map: dict = None) -> easypost.Shipment:
+            _map = EasyPostDevTools.Shipments.get_return_map(to_address_map, from_address_map, parcel_map)
             return easypost.Shipment.create(**_map)
 
         @staticmethod
